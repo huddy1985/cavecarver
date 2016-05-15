ceca0473fc7b1a30cbbb5478e592f4ef5ab5a408
Konrad Eisele <eiselekd@gmail.com>
2016-05-15 18:42:02 +0200

    all

--- qemu.sh
+++ qemu.sh
@@ -1,2 +1,4 @@
-/home/eiselekd/bin/bin/qemu-system-x86_64 -hda linux-tinycore-3.4.img -m 1024 -net nic -net user -rtc clock=vm,base=1999-12-31 -enable-kvm -qmp unix:/tmp/socket,server,nowait
-
+qemu-system-x86_64  -hda linux-tinycore-3.4.img -m 1024 -net nic -net user -rtc clock=vm,base=1999-12-31 -enable-kvm -qmp unix:/tmp/socket,server,nowait \
+		-machine "kernel_irqchip=off"  -monitor stdio 
+#-machine "kernel_irqchip=on"
+#qemu-kvm
--- qemu_cap.sh
+++ qemu_cap.sh
@@ -1,4 +1,4 @@
-/home/eiselekd/bin/bin/qemu-system-x86_64 -hda linux-tinycore-3.4.img -m 1024 -net nic -net user -rtc clock=vm,base=1999-12-31 -enable-kvm -qmp unix:/tmp/socket,server,nowait & sleep 5; echo \
+qemu-system-x86_64 -hda linux-tinycore-3.4.img -m 1024 -net nic -net user -rtc clock=vm,base=1999-12-31 -enable-kvm -qmp unix:/tmp/socket,server,nowait & sleep 5; echo \
 '{"execute":"qmp_capabilities"}'\
 '{"execute":"qom-get","arguments":{"path":"/machine/unattached/device[0]","property":"feature-words"},"id":"feature-words"}'\
 '{"execute":"qom-get","arguments":{"path":"/machine/unattached/device[0]","property":"family"},"id":"family"}' \
@@ -7,3 +7,6 @@
 '{"execute":"qom-get","arguments":{"path":"/machine/unattached/device[0]","property":"model-id"},"id":"model-id"}'\
 '{"execute":"qom-get","arguments":{"path":"/machine/unattached/device[0]","property":"tsc-frequency"},"id":"tsc-frequency"}'\
 '{"execute":"quit"}' | nc -U /tmp/socket
+
+
+#'{"execute":"qom-get","arguments":{"path":"/machine","property":"kernel-irqchip"},"id":"kernel-irqchip"}'\
