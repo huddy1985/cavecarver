ca98a1b2e9bddcf9b70acf2ce39d74471257646f
Konrad Eisele <eiselekd@gmail.com>
2016-05-19 16:41:57 +0200

    all

--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,6 @@
 qemu-system-x86_64  -hda linux-tinycore-3.4.img -m 1024 -net nic -net user -rtc clock=vm,base=1999-12-31 -enable-kvm -qmp unix:/tmp/socket,server,nowait \
-		-machine "kernel_irqchip=off"  -monitor stdio 
+		    -machine "kernel_irqchip=off"
+
+#-monitor stdio 
 #-machine "kernel_irqchip=on"
 #qemu-kvm
