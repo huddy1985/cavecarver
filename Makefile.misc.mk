4231300b0c65fd697de30d89a5b8db4aae11a29c
Konrad Eisele <eiselekd@gmail.com>
2016-08-22 18:32:23 +0200

    all

--- Makefile.misc.mk
+++ Makefile.misc.mk
@@ -1,5 +1,6 @@
 
 linux:
+	#git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/yakkety
 	git clone git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial ubuntu-linux
 	cd ubuntu-linux; fakeroot debian/rules clean
 	cd ubuntu-linux; fakeroot debian/rules binary-headers binary-generic skipdbg=false
