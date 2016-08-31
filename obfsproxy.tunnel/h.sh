e344c0307f7702cf19638981099f35200a63a7de
Konrad Eisele <eiselekd@gmail.com>
2016-08-31 19:11:05 +0200

    all

--- obfsproxy.tunnel/h.sh
+++ obfsproxy.tunnel/h.sh
@@ -2,12 +2,14 @@
 . ${HOME}/.o/p.sh
 d=`dirname $(realpath $0)`
 cat ${d}/standalone_client_config.ini          | \
-    sed -e 's:--server--:${server}:g '         | \
-    sed -e 's:--serverport--:${serverport}:g ' | \
-    sed -e 's:--cert--:${cert}:g '  \
+    sed -e "s:--server--:${server}:g "         | \
+    sed -e "s:--serverport--:${serverport}:g " | \
+    sed -e "s:--cert--:${cert}:g "  | \
+    sed -e "s:--state--:${HOME}/.o:g " | \
+    sed -e "s:--obfs--:${obfs}:g " \
     > ${HOME}/.o/c.ini
 
 killall obfs4proxy 2> /dev/null
 (cd ${d}; python3 standalone_client.py -v ${HOME}/.o/c.ini) &
-
+sleep 1
 echo "Tunnel to ${server}:${serverport}, ssh gate at 127.0.0.1:8000"
--- obfsproxy.tunnel/s.sh
+++ obfsproxy.tunnel/s.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 . ${HOME}/.o/p.sh
 d=`dirname $(realpath $0)`
-cat ${d}/standalone_client_config.ini          | \
-    sed -e 's:--server--:${server}:g '         | \
-    sed -e 's:--serverport--:${serverport}:g ' | \
-    sed -e 's:--serverlocal--:${serverlocal}:g ' | \
-    sed -e 's:--node-id--:${node-id}:g ' | \
-    sed -e 's:--private-key--:${private-key}:g ' | \
-    sed -e 's:--public-key--:${public-key}:g ' | \
-    sed -e 's:--drbg-seed--:${drbg-seed}:g ' | \
-    sed -e 's:--cert--:${cert}:g '  \
+cat ${d}/standalone_server_config.ini          | \
+    sed -e "s:--server--:${server}:g "         | \
+    sed -e "s:--serverport--:${serverport}:g " | \
+    sed -e "s:--serverlocal--:${serverlocal}:g " | \
+    sed -e "s:--node-id--:${nodeid}:g " | \
+    sed -e "s:--private-key--:${privatekey}:g " | \
+    sed -e "s:--public-key--:${publickey}:g " | \
+    sed -e "s:--drbg-seed--:${drbgseed}:g " | \
+    sed -e "s:--cert--:${cert}:g " | \
+    sed -e "s:--state--:${HOME}/.o:g " | \
+    sed -e "s:--obfs--:${obfs}:g " \
     > ${HOME}/.o/s.ini
 
 killall obfs4proxy 2> /dev/null
 (cd ${d}; python3 standalone_server.py -v ${HOME}/.o/s.ini) &
+sleep 1
 echo "Tunnel at ${serverlocal}:${serverport}, external ${server}::${serverport}"
