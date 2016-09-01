114ee085b00bea1b36d90a1bc7dc949b2a7a9e71
Konrad Eisele <eiselekd@gmail.com>
2016-09-01 15:53:56 +0200

    all

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# dumpbin of random stuff
+
 # cavecarver
 Use .gnu.version section  in an elf executable as container for custom code.
 
---
+++ ubuntu/recompile.sh
@@ -0,0 +1,10 @@
+rm -rf linux-3.19.0
+
+git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial
+git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/yakkety
+
+apt-get source linux-image-`uname -r`
+cd linux-3.19.0/; \
+fakeroot debian/rules clean; \
+DEB_BUILD_OPTIONS=parallel=4 AUTOBUILD=1 NOEXTRAS=1 fakeroot debian/rules binary-generic binary
+
