8cd84ff9b79ee2efb5bc112ac4a6a4dddc78fc0b
Konrad Eisele <eiselekd@gmail.com>
2016-10-18 13:52:33 +0200

    all

--- ubuntu/recompile.sh
+++ ubuntu/recompile.sh
@@ -1,13 +1,19 @@
-rm -rf linux-3.19.0
+#rm -rf linux-3.19.0
 
-git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial
-git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/yakkety
+#git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/xenial
+#git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/yakkety
+#apt-get source linux-image-4.8.0-22-generic
+#fakeroot debian/rules editconfigs
+
+#amd64/config.flavour.generic
+#amd64/config.flavour.lowlatency
+#chmod a+x linux-4.8.0/debian/scripts/misc/splitconfig.pl
 
 apt-get source linux-image-`uname -r`
-cd linux-3.19.0/; \
+cd linux-4.8.0/; \
 fakeroot debian/rules clean; \
 DEB_BUILD_OPTIONS=parallel=4 AUTOBUILD=1 NOEXTRAS=1 fakeroot debian/rules binary-generic binary
 
-export DEB_BUILD_OPTIONS="debug nostrip noopt"
-dpkg-buildpackage -rfakeroot -b
+#export DEB_BUILD_OPTIONS="debug nostrip noopt"
+#dpkg-buildpackage -rfakeroot -b
 
