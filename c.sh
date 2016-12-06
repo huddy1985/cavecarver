cc716c9a7325757038415a3c6b82d0fafaacfb47
Konrad Eisele <eiselekd@gmail.com>
2016-12-06 19:32:58 +0100

    all

--- c.sh
+++ c.sh
@@ -9,3 +9,10 @@ cryptsetup luksUUID /dev/sdx
 # automount:
 # sudo cryptsetup luksAddKey /dev/sdx keyfile
 # echo "crypt-mapper UUID=`cryptsetup luksUUID /dev/sdx` keyfile luks" >> /etc/crypttab
+
+
+sudo mkfs.ext4 /dev/sda3
+sudo cryptsetup -c aes-xts-plain -s 256 luksFormat /dev/sda3
+sudo cryptsetup luksOpen /dev/sda3 luksroot
+sudo mkfs.btrfs /dev/mapper/luksroot
+sudo mount -o noatime,nodiratime,ssd,compress=lzo /dev/mapper/luksroot /mnt
