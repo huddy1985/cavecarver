72ac4498358244611b6d3557bb6e1ad13d3754b2
Konrad Eisele <eiselekd@mail.com>
2018-09-23 10:45:23 +0200

    all

--- shinter/mod/test/process.py
+++ shinter/mod/test/process.py
@@ -3,7 +3,8 @@ import re, sys, argparse, os
 entry_re = re.compile("([0-9]+)\.([0-9]+):([0-9]+\.[0-9]+):(.*)");
 fork_re = re.compile("fork ([0-9]+)->([0-9]+)");
 free_re = re.compile("free ([0-9]+)");
-execve_re = re.compile("\+ ([0-9]+)\.([0-9]+):execve:(.*)");
+execve_start_re = re.compile("start execve:([0-9]+)\.([0-9]+):(.*)");
+execve_stop_re = re.compile("stop execve:([0-9]+)\.([0-9]+):(.*)");
 
 LOG_NAME = (1<<0)
 LOG_ARG  = (1<<1)
@@ -14,9 +15,8 @@ LOG_FORK = (1<<5)
 LOG_CONT = (1<<8)
 
 class pidobj(object):
-    def __init__(self,ppid,pid,idx):
-        if not (ppid is None):
-            ppid.childs.append(self)
+    def __init__(self,up,ppid,pid,idx):
+        self.up = up
         self.idx = idx
         self._ppid = ppid
         self._pid = pid
@@ -31,9 +31,16 @@ class pidobj(object):
         self.output();
         for c in self.childs:
             c.close()
+    def stop(self):
+        pass
+
+    def tryoutput(self):
+        pass
 
     def output(self):
         if self.outputted:
+            return
+        if (not self.isexecve()) and not self.haschildren():
             return
         p = self.getFilePath()
         try:
@@ -49,16 +56,42 @@ class pidobj(object):
     def getFileName(self):
         if self.execve is None:
             return "undef"
-        return self.execve.replace("/","_")
+        p = os.path.basename(self.execve)
+        return p.replace("/","_")
     def getFilePath(self):
-        p = ("06%d_%s" %(self.idx, self.getFileName()))
-        if not (self._ppid is None):
-            p = os.path.join(self._ppid.getFilePath(),p)
+        par = self.getparent()
+        if (not self.isexecve()) and self.hasparent() and not (par is None): #jump over undef
+            return par.getFilePath()
+        p = ("%06d_%s" %(self.idx, self.getFileName()))
+        par = self.getparent()
+        if not (par is None):
+            p = os.path.join(par.getFilePath(),p)
         return p
     def setcwd(self,p):
         self.cwd = p
     def setexecve(self,n):
         self.execve = n
+    def setparent(self,p):
+        self._ppid = p
+            
+    def isexecve(self):
+        return not (self.execve is None)
+    
+    def getparent(self):
+        if (self._ppid is None):
+            return None
+        return self.up.getpid(self._ppid)
+    
+    def hasparent(self):
+        return not (self._ppid is None)
+    def haschildren(self):
+        return len(self.childs) > 0
+    def checkpids(self,ppid,pid):
+        p = self.getparent()
+        if not (p is None):
+            if not (p._pid == ppid):
+                return False
+        return (self._pid == pid)
     def addarg(self,e):
         self.args.insert(0, e)
     def addenv(self,e):
@@ -97,26 +130,49 @@ class process(object):
         for f in self.pids:
             f.output();
 
-    def getpid(self, pid):
+    def getpid(self, pid, generate=False):
         if pid in self.pid:
             return self.pid[pid]
+        if generate:
+            return self.newpid(None, pid)
         return None
+
+    def newpid(self, ppid, pid):
+        o = pidobj(self,ppid, pid, self.idx)
+        self.pid[pid] = o
+        self.pids.append(o)
+        self.idx += 1
+        return o
 
     def closepid(self,pid):
         if pid in self.pid:
             pobj = self.pid[pid]
             pobj.close();
 
-    def openpid(self, ppid, pid):
-        pobj = None
-        if ppid in self.pid:
-            pobj = self.pid[ppid]
+    # if the pid is reused via a execv (two in a row) then close the previouse
+    def startexecv(self, ppid, pid, cmd):
+        o = None
         if pid in self.pid:
-            self.pid[pid] = None
-        o = pidobj(pobj, pid, self.idx)
-        self.pids.append(o)
+            if self.pid[pid].isexecve():
+                if (not self.pid[pid].checkpids(ppid,pid)):
+                    pass #err
+                self.pid[pid].stop()
+            else:
+                o = self.pid[pid] # reuse pid obj
+        if o is None:
+            o = self.newpid(ppid, pid)
         self.pid[pid] = o
-        self.idx += 1#
+        o.setexecve(cmd);
+        o.setparent(ppid)
+
+    def stopexecv(self, ppid, pid, cmd):
+        if not (pid in self.pid):
+            self.startexecv(ppid, pid, cmd)
+        self.pid[pid].tryoutput()
+
+    def openpid(self, ppid, pid):
+        if pid in self.pid:
+            pass
 
     def run(self):
         prev = -1
@@ -148,12 +204,17 @@ class process(object):
         if self._prev is None:
             return
         (typ,pid,stamp,rest) = self._prev
-        pid = self.getpid(pid)
+        pid = self.getpid(pid, generate=True)
         self._prev = None
         if (typ == LOG_NAME and pid):
-            m = execve_re.match(rest)
-            if (m):
-                pid.setexecve(m.group(3))
+            m0 = execve_start_re.match(rest)
+            m1 = execve_stop_re.match(rest)
+            if (m0):
+                (eppid,epid,ecmd) = (int(m0.group(1)),int(m0.group(2)),m0.group(3))
+                self.startexecv(eppid,epid,ecmd)
+            elif (m1):
+                (eppid,epid,ecmd) = (int(m1.group(1)),int(m1.group(2)),m1.group(3))
+                self.stopexecv(eppid,epid,ecmd);
             else:
                 pid.setexecve(rest)
         elif (typ == LOG_ARG and pid):
